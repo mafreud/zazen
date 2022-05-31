d7689046993b14f36bfba950d01a6f216123d00b
Masahiro Aoki <rafe.kun@gmail.com>
2022-05-31 18:53:14 +0100

    update

--- lib/dashboard/dashboard_page.dart
+++ lib/dashboard/dashboard_page.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:zazen/utilities/go_router/router.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(title: const Text('AppBar')),
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent[700],
+        title: const Text('Dashboard - where issues get together'),
+      ),
       backgroundColor: Colors.grey[900],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('hey'),
-            ElevatedButton(
-              onPressed: () {
-                context.goNamed(
-                  AppRoute.issueDetail.name,
-                  params: {'id': '12345'},
-                );
-              },
-              child: const Text('Go'),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                width: 200,
+                height: height * 0.8,
+                color: Colors.teal,
+                child: const Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(height: 100, child: Card())),
+              ),
             )
           ],
         ),
