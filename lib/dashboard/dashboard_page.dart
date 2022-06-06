0e9d7937d721c3ee0fb7c202a84c8aa6e16077f0
Masahiro Aoki <rafe.kun@gmail.com>
2022-06-06 17:04:38 +0100

    refactor

--- lib/dashboard/dashboard_page.dart
+++ lib/dashboard/dashboard_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:github_sign_in/github_sign_in.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -8,6 +7,22 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: TextButton(
+                child: const Text('SignOut'),
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.deepPurpleAccent[700],
         title: const Text('Dashboard - where issues get together'),
@@ -16,54 +31,7 @@ class DashboardPage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                // Create a GitHubSignIn instance
-                final GitHubSignIn gitHubSignIn = GitHubSignIn(
-                  clientId: const String.fromEnvironment('GITHUB_CLIENT_ID'),
-                  clientSecret:
-                      const String.fromEnvironment('GITHUB_CLIENT_SECRET'),
-                  redirectUrl:
-                      'https://zazen-release.firebaseapp.com/__/auth/handler',
-                );
-
-                // Trigger the sign-in flow
-                final result = await gitHubSignIn.signIn(context);
-
-                // Create a credential from the access token
-                final githubAuthCredential =
-                    GithubAuthProvider.credential(result.token!);
-
-                // Once signed in, return the UserCredential
-                await FirebaseAuth.instance
-                    .signInWithCredential(githubAuthCredential);
-              },
-              child: const Text('native login'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                GithubAuthProvider githubProvider = GithubAuthProvider();
-
-                await FirebaseAuth.instance.signInWithPopup(githubProvider);
-              },
-              child: const Text('fetch'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                print(user!.uid);
-              },
-              child: const Text('fetch'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text('signout'),
-            ),
-          ],
+          children: const [],
         ),
       ),
     );
